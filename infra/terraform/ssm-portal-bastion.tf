c400269b39ae9b65ac9c0445fb2940c554657990
Từ Phúc Nguyên <tuphucnguyen20051@gmail.com>
2026-07-13 22:33:21 +0700

    fix(deploy): increase Bastion Host root volume size to 30GB (#108)
    Change root block device volume size from 8GB to 30GB to satisfy the minimum snapshot size requirements of the Amazon Linux 2023 AMI in us-east-1.

--- infra/terraform/ssm-portal-bastion.tf
+++ infra/terraform/ssm-portal-bastion.tf
@@ -118,7 +118,7 @@ resource "aws_instance" "portal_bastion" {
   })
 
   root_block_device {
-    volume_size           = 8
+    volume_size           = 30
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
