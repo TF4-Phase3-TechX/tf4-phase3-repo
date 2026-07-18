311ca50b41c1971114e532a20447021a709a401c
ArtinaxW <161110817+BuiThanhNghiaDTU19122004@users.noreply.github.com>
2026-07-18 17:40:30 +0700

    fix(cdo07): fix output eventbridge rule

--- infra/terraform/modules/security-slack-alerts/outputs.tf
+++ infra/terraform/modules/security-slack-alerts/outputs.tf
@@ -3,9 +3,14 @@ output "sns_topic_arn" {
   value       = aws_sns_topic.alerts.arn
 }
 
-output "eventbridge_rule_a_arn" {
-  description = "ARN of the EventBridge Rule for CloudTrail events"
-  value       = aws_cloudwatch_event_rule.cloudtrail_alerts.arn
+output "eventbridge_rule_global_arn" {
+  description = "ARN of the EventBridge Rule for Global CloudTrail events"
+  value       = aws_cloudwatch_event_rule.cloudtrail_alerts_global.arn
+}
+
+output "eventbridge_rule_regional_arn" {
+  description = "ARN of the EventBridge Rule for Regional CloudTrail events"
+  value       = aws_cloudwatch_event_rule.cloudtrail_alerts_regional.arn
 }
 
 output "eventbridge_rule_b_arn" {
