ffcc1bd71419f7a2c3fb70ecb210340d7266a4a9
bearh141 <dhoang1401@gmail.com>
2026-07-24 09:42:26 +0700

    style(terraform): format eks-audit-firehose.tf according to terraform fmt rules

--- infra/terraform/eks-audit-firehose.tf
+++ infra/terraform/eks-audit-firehose.tf
@@ -332,10 +332,9 @@ resource "aws_kinesis_firehose_delivery_stream" "eks_audit_logs" {
     role_arn   = aws_iam_role.firehose_to_s3.arn
     bucket_arn = aws_s3_bucket.eks_audit_logs.arn
 
-    buffering_size     = 5  # MB (1-128)
-    buffering_interval = 60 # Seconds (60-900)
-    compression_format = "UNCOMPRESSED" # Evades double-gzip
-
+    buffering_size      = 5  # MB (1-128)
+    buffering_interval  = 60 # Seconds (60-900)
+    compression_format  = "UNCOMPRESSED" # Evades double-gzip
     prefix              = "!yyyy/!MM/!dd/!HH/"
     error_output_prefix = "errors/!yyyy/!MM/!dd/!HH/!error-output-type/"
 
